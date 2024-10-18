4a93af0f1bff3149f048ef646010914040ce580c
JEM1X <62144383+JEM1X@users.noreply.github.com>
2024-10-18 11:29:59 +0300

    0.3.5
    дегенерация

--- Assets/LevelGeneration/LevelGenerator.cs
+++ Assets/LevelGeneration/LevelGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -9,11 +10,38 @@ public class LevelGenerator : MonoBehaviour
     public int pathLength;
     public int iterationNumber;
     public GameObject[] rooms = new GameObject[4];
+    public GameObject wall;
     private HashSet<Vector2Int> takenRooms = new HashSet<Vector2Int> {Vector2Int.zero};
 
     private void Start()
     {
         GenrateLevel();
+        GenerateWalls();
+    }
+
+    private void GenerateWalls()
+    {
+        foreach(Vector2Int v in takenRooms)
+        {
+            Vector3 wallPosition = new Vector3(v.x * gridSize, v.y * gridSize, 0);
+
+            if (!takenRooms.Contains(v + Vector2Int.down))
+            {
+                Instantiate(wall, wallPosition, Quaternion.identity, transform);
+            }
+            if (!takenRooms.Contains(v + Vector2Int.right))
+            {
+                Instantiate(wall, wallPosition, Quaternion.Euler(new Vector3(0, 0, 90)), transform);
+            }
+            if (!takenRooms.Contains(v + Vector2Int.left))
+            {
+                Instantiate(wall, wallPosition, Quaternion.Euler(new Vector3(0, 0, -90)), transform);
+            }
+            if (!takenRooms.Contains(v + Vector2Int.up))
+            {
+                Instantiate(wall, wallPosition, Quaternion.Euler(new Vector3(0, 0, 180)), transform);
+            }
+        }
     }
 
     private void GenrateLevel()
@@ -86,30 +114,47 @@ public class LevelGenerator : MonoBehaviour
 
     private GameObject GetRandomRoom(Vector2Int position)
     {
-        GameObject room = null;
+        GameObject room = rooms[0];
 
-        switch(UnityEngine.Random.Range(0, 4))
+        // Check if the room can be placed without intersections
+        switch (UnityEngine.Random.Range(0, 4))
         {
-            case 0:
-                room = rooms[0]; 
-                takenRooms.Add(position);
+            case 0: // 1x1 room
+                {
+                    room = rooms[0];
+                    takenRooms.Add(position);  // Mark this position as taken
+                }
                 break;
-            case 1:
-                room = rooms[1];
-                takenRooms.Add(position);
-                takenRooms.Add(position + Vector2Int.up);
+
+            case 1: // 1x2 room
+                if (!takenRooms.Contains(position + Vector2Int.up))
+                {
+                    room = rooms[1];
+                    takenRooms.Add(position);
+                    takenRooms.Add(position + Vector2Int.up);  // Mark both positions as taken
+                }
                 break;
-            case 2:
-                room = rooms[2];
-                takenRooms.Add(position);
-                takenRooms.Add(position + Vector2Int.right);
+
+            case 2: // 2x1 room
+                if (!takenRooms.Contains(position + Vector2Int.right))
+                {
+                    room = rooms[2];
+                    takenRooms.Add(position);
+                    takenRooms.Add(position + Vector2Int.right);  // Mark both positions as taken
+                }
                 break;
-            case 3:
-                room = rooms[3];
-                takenRooms.Add(position);
-                takenRooms.Add(position + Vector2Int.up);
-                takenRooms.Add(position + Vector2Int.right);
-                takenRooms.Add(position + Vector2Int.right + Vector2Int.up);
+
+            case 3: // 2x2 room
+                if (!takenRooms.Contains(position + Vector2Int.up) &&
+                    !takenRooms.Contains(position + Vector2Int.right) &&
+                    !takenRooms.Contains(position + Vector2Int.right + Vector2Int.up))
+                {
+                    room = rooms[3];
+                    takenRooms.Add(position);
+                    takenRooms.Add(position + Vector2Int.up);
+                    takenRooms.Add(position + Vector2Int.right);
+                    takenRooms.Add(position + Vector2Int.right + Vector2Int.up);  // Mark all 4 positions as taken
+                }
                 break;
         }
 

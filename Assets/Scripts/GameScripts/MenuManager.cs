ca7286ea210bb619ae680bd5a7b4653da1c1ce38
scout2281337 <146185803+scout2281337@users.noreply.github.com>
2024-10-27 21:54:50 +0300

    0.4.7
    теперь у нас есть gamemanager как независимый обьект и он синглтон у нас

--- Assets/Scripts/GameScripts/MenuManager.cs
+++ Assets/Scripts/GameScripts/MenuManager.cs
@@ -5,54 +5,57 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public static MenuManager Instance { get; private set; } // Singleton Instance
     public GameObject menu;
-    
-    void Start()
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            //DontDestroyOnLoad(gameObject); // Save when we load new scene
+        }
+        else
+        {
+            Destroy(gameObject); // Destroy, if we have copy
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if ( Input.GetKeyDown(KeyCode.P) && SceneManager.GetActiveScene().name == "TestRoom"  )
+        if (Input.GetKeyDown(KeyCode.P) && SceneManager.GetActiveScene().name == "TestRoom")
         {
             SceneTransition.SwitchToScene("Mainmenu");
         }
-        if ( Input.GetKeyDown(KeyCode.Escape)) 
-        {
 
-            menu.SetActive(!menu.activeSelf);
-            
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleMenu();
         }
     }
 
-    public void GoToGame() 
+    public void GoToGame()
     {
         SceneTransition.SwitchToScene("Mainmenu");
     }
 
-    public void ToMainMenu() 
+    public void ToMainMenu()
     {
         SceneManager.LoadScene(0);
-    
-    }
-
-    public void OpenMenu() 
-    {
-        menu.SetActive(!menu.activeSelf);
     }
 
     public void StartGame()
     {
         SceneTransition.SwitchToScene("TestRoom");
-
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
 
+    public void ToggleMenu()
+    {
+        menu.SetActive(!menu.activeSelf);
     }
 }

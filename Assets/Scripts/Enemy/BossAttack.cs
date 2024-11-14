17b57286fd34b31bc08dc36db38059f1278a6443
scout2281337 <146185803+scout2281337@users.noreply.github.com>
2024-11-14 14:51:18 +0300

    0.5.7

--- Assets/Scripts/Enemy/BossAttack.cs
+++ Assets/Scripts/Enemy/BossAttack.cs
@@ -17,24 +17,17 @@ public class BossAttack : EnemyAttack
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-        
-
-
-
-    }
+    
     protected override void Shoot()
     {
         if (!canShoot) return;
 
-        if (enemyHealth.currentHealth >= 300)
+        if (enemyHealth.currentHealth >= 0.8 * enemyHealth.maxHealth)
         {
-            // Выполняем каждую стратегию атаки
+            projectileSpeed = 7f;
             attackStrategies[0].ExecuteAttack(firePoint, projectilePrefab, player, projectileSpeed, numberOfProjectiles);
         }
-        else if(enemyHealth.currentHealth >= 150) 
+        else if(enemyHealth.currentHealth >= 0.5 * enemyHealth.maxHealth) 
         {
             fireRate = 0.8f;
             attackStrategies[1].ExecuteAttack(firePoint, projectilePrefab, player, projectileSpeed, numberOfProjectiles);

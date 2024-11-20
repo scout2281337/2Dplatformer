11f58686d369771e4a984f7cd70c0cb15244f2c4
JEM1X <62144383+JEM1X@users.noreply.github.com>
2024-11-20 15:34:40 +0300

    0.6.2
    Сделал более менее норм комнаты

--- Assets/Scripts/OneWayPlatform.cs
+++ Assets/Scripts/OneWayPlatform.cs
@@ -10,7 +10,7 @@ public class OneWayPlatform : MonoBehaviour
 
     private void Start()
     {
-        player = PlayerManager.Instance.player; // Найти игрока по тегу
+        player = PlayerManager.Instance.player;
         playerCollider = player.GetComponent<CapsuleCollider2D>();
         platformCollider = GetComponent<BoxCollider2D>();
     }
